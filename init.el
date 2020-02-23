6612e3704827a770d3beedccd75128ba070faf4a
Michael Bruce <me@michaelbruce.co>
2020-02-23 16:10:52 +0000

    default indent level of 2

--- init.el
+++ init.el
@@ -4,21 +4,22 @@
       garbage-collection-messages t)
 
 (setq enabled '(ido-mode show-paren-mode electric-pair-mode
-			 electric-indent-mode global-auto-revert-mode
-			 savehist-mode))
+                         electric-indent-mode global-auto-revert-mode
+                         savehist-mode))
 (setq disabled '(tool-bar-mode menu-bar-mode scroll-bar-mode fringe-mode))
 (setq-default
  split-height-threshold 1200
  split-width-threshold 2000
  indent-tabs-mode nil
- debug-on-error t
+ tab-width 2
+ kill-whole-line t
  inhibit-splash-screen t
  vc-follow-symlinks t
  column-number-mode t
  load-prefer-newer t
  ring-bell-function 'ignore
  fill-column 80
- show-trailing-whitespace t
+ show-trailing-whitespace t ;; only for certain modes?
  compilation-ask-about-save nil
  backup-directory-alist `(("." . ,(concat user-emacs-directory "saves")))
  custom-file (make-temp-file ""))
@@ -27,6 +28,14 @@
 
 (dolist (m enabled) (funcall m 1))
 (dolist (m disabled) (funcall m -1))
+
+(defun set-indent (n)
+  (setq-local tab-width n)
+  (setq-local c-basic-offset n)
+  (setq-local javascript-indent-level n)
+  (setq-local js-indent-level n)
+  (setq-local js2-basic-offset n)
+  (setq-local css-indent-offset n))
 
 (defmacro ifn (fn)
   `(lambda () (interactive) ,fn))
@@ -38,31 +47,6 @@
      (unless (package-installed-p ',name)
        (package-install ',name))
      (require ',name)))
-
-;; third party code
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
-
-(when (include haskell-mode)
-  (add-hook 'haskell-mode-hook 'interactive-haskell-mode))
-;; haskell-mode must have started first
-(include markdown-mode)
-(include magit)
-
-(defun code-config ()
-  (if (version<= "26.0.50" emacs-version)
-      (display-line-numbers-mode 1)
-    (linum-mode 1)))
-
-(dolist (hook '(prog-mode-hook css-mode-hook)) (add-hook hook 'code-config))
-
-;; (include display-line-number-mode)
-;; (include interactive-haskell-mode)
-;; (setq package-check-signature nil)
-;; intero?
-
 (dolist
     (binding
      `(("M-o" . other-window)
@@ -90,3 +74,38 @@
 	  (lambda ()
 	    (define-key ido-completion-map (kbd "C-w") 'backward-kill-word)
 	    (define-key ido-file-completion-map (kbd "C-w") 'ido-delete-backward-word-updir)))
+
+;; third party code -> we might make IO calls, anything after here is expected to fail.
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(include diminish)
+
+(include company)
+(add-hook 'after-init-hook 'global-company-mode)
+(diminish 'company-mode)
+
+(when (include haskell-mode)
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode))
+;; haskell-mode must have started first
+(include markdown-mode)
+(include magit)
+
+(defun code-config ()
+  (if (version<= "26.0.50" emacs-version)
+      (display-line-numbers-mode 1)
+    (linum-mode 1))
+  (set-indent 2))
+
+(defun default-config ()
+  (set-indent 2))
+
+(add-hook 'text-mode-hook 'default-config)
+(dolist (hook '(prog-mode-hook css-mode-hook)) (add-hook hook 'code-config))
+
+;; (include display-line-number-mode)
+;; (include interactive-haskell-mode)
+;; (setq package-check-signature nil)
+;; intero?
